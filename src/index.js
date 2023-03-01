c2dfdab780f2aeb7a262f12e7085d434fdcd5a9a
Liz Ainslie <lizainslie16@gmail.com>
2023-02-28 22:22:14 -0700

    feat(api): vercel deez nuts

--- src/index.js
+++ src/index.js
@@ -1,24 +1,16 @@
 const path = require('path');
 const express = require('express');
 
-const configPath = path.join(__dirname, '..', 'config.json');
 const config = require(configPath);
 
 const app = express();
 
-const githubUser = config.githubUser || 'LizAinslie';
+const githubUser = process.env.GITHUB_USER;
 app.get('*', (req, res) => {
   const repoPath = req.url;
   res.set('location', `https://github.com/${githubUser}${repoPath}`);
   res.status(301).send();
 });
-
-if (process.env.NODE_ENV === 'development') {
-  const port = config.port || 3000;
-  app.listen(port, () => {
-    console.log(`Server listening on port ${port}.`);
-  });
-}
 
 // vercel
 module.exports = app;
